9e4602da4490336f4db12dc05d99c86bdb46569e
Guilherme Jacob Licinio Dalmarco <dalmarcogd@gmail.com>
2017-12-08 15:56:23 -0200

    test

---
+++ src/main/java/module-info.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+/**
+ * @author data52311
+ *
+ */
+module gdalmarco.testelasticsearch {
+	exports gdalmarco.testelasticsearch;
+	
+	requires spring.core;
+    requires spring.beans;
+    requires spring.context;
+    requires spring.aop;
+    requires spring.web;
+    requires spring.expression;
+
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+}
